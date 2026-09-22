ff1de7355293b5c5fe8ec08a84825da4677309d5
LucasMatsuta <stockfreeshop2010@gmail.com>
2026-09-22 09:35:24 -0300

    Coloque o pulo e camer no player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //Vai reconhecer o movimento horizontal
+
+        rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); //Vai aplicar a velocidade horizontal ao Rigidbody2D
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+          rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); //Vai aplicar uma força vertical ao Rigidbody2D
+        }
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;// quando o jogador estiver com o player tocando no chão
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;// quando o jogador não estiver com o player tocando no chão
+        }
+    }
+
+}
